568acf03fb8efe7f112cda2aa14ae379c681f4cf
Németh Milán <nemethmilan0424@gmail.com>
2023-11-14 15:30:29 +0100

    Fix
    Title size change

--- src/Componenets/Navbar/Navbar.jsx
+++ src/Componenets/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center border-b border-gray-100 w-full px-44 py-2">
 
             {/* Logo and Title */}
-            <div className="text-5xl font-extrabold text-gray-900 dark:text-white font-roboto">
+            <div className="text-xl font-extrabold text-gray-900 dark:text-white font-roboto">
                 {/* Title with Color Transition */}
                 <span className="text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-blue-400">
                     Furry Tails
