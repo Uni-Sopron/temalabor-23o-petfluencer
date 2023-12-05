4c0c7e43037ce646d0a2ef81906cc9b3f9ced87e
Balázs Valentin Leyrer <90619307+LeyrerBalazs@users.noreply.github.com>
2023-12-06 00:18:40 +0100

    Update PostReducer.jsx

--- src/Componenets/AppContext/PostReducer.jsx
+++ src/Componenets/AppContext/PostReducer.jsx
@@ -1,6 +1,7 @@
 export const postActions = {
     SUBMIT_POST: 'SUBMIT_POST',
     HANDLE_ERROR: 'HANDLE_ERROR',
+    ADD_LIKE: 'ADD_LIKE',
 };
 
 export const postsStates = {
@@ -15,6 +16,12 @@ export const PostReducer = (state, action) => {
                 ...state,
                 error: false,
                 posts: action.posts,
+            };
+        case postActions.ADD_LIKE:
+            return {
+                ...state,
+                error: false,
+                likes: action.likes,
             };
         case postActions.HANDLE_ERROR:
             return {
