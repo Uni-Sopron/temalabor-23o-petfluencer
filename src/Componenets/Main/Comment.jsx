d11e430e4df96b210602f5762db5da04ba61c4a2
Balázs Valentin Leyrer <90619307+LeyrerBalazs@users.noreply.github.com>
2023-12-23 18:11:29 +0100

    Update Comment.jsx

--- src/Componenets/Main/Comment.jsx
+++ src/Componenets/Main/Comment.jsx
@@ -1,17 +1,13 @@
 import { Avatar } from '@material-tailwind/react'
 import avatar from './../../assets/avatar.png'
-import React, { useContext, useRef } from 'react'
-import { AuthContext } from '../AppContext/AppContext'
+import React, { useEffect } from 'react'
 
 const Comment = ({ name, comment, image }) => {
-    
+  
   return (
     <div className='flex items-center mt-2 w-full'>
         <div className='mx-2'>
-            <Avatar size='sm'
-                    variant='circular' 
-                    src={image || avatar}
-                    alt='avatar'></Avatar>
+            <Avatar size='sm' variant='circular' src={image || avatar} alt='avatar'></Avatar>
         </div>
         <div className='flex flex-col items-start bg-gray-100 rounded-2x1 p-1 max-w-[600px]'>
             <p className='font-roboto text-black text-sm no-underline tracking-normal leading-none p-1 font-medium'>{name}</p>
