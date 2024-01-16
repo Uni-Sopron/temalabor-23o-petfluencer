745ec3305f81094fc01efc5bf35109f9951b42ae
Németh Milán <nemethmilan0424@gmail.com>
2024-01-16 20:24:01 +0100

    Update
    Hashtag update

--- src/Componenets/LeftSidebar/LeftSide.jsx
+++ src/Componenets/LeftSidebar/LeftSide.jsx
@@ -1,25 +1,17 @@
-import React, { useRef, useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Tooltip } from "@material-tailwind/react";
 import { Avatar } from "@material-tailwind/react";
 import animal from "../../assets/animal.jpg";
 import avatar from "../../assets/avatar.png";
 import facebook from "../../assets/facebook.png";
 import twitter from "../../assets/twitter.png";
-import laptop from "../../assets/laptop.jpg";
-import media from "../../assets/media.jpg";
-import apps from "../../assets/apps.jpg";
-import dogmarket from "../../assets/dogmarket.png";
 import { AuthContext } from "../AppContext/AppContext";
-import { collection, setDoc } from "firebase/firestore";
+import { collection, } from "firebase/firestore";
 import { db } from "../Firebase/firebase";
-import { HANDLE_ERROR } from "../AppContext/PostReducer";
-import { collectionUsersRef } from "../AppContext/AppContext";
 import { getDocs, query, where } from "firebase/firestore";
 import { updateDoc } from "firebase/firestore";
 
 const LeftSide = () => {
-  const [data, setData] = useState([]);
-  const count = useRef(0);
   const { user, userData } = useContext(AuthContext);
   const [kind, setKind] = useState("");
   const [species, setSpecies] = useState("");
@@ -27,75 +19,20 @@ const LeftSide = () => {
   const [habitat, setHabitat] = useState("");
   const [description, setDescription] = useState("");
 
-  const handleRandom = (arr) => {
-    setData(arr[Math.floor(Math.random() * arr?.length)]);
-  };
-
-  useEffect(() => {
-    const imageList = [
-      {
-        id: "1",
-        image: laptop,
-      },
-      {
-        id: "2",
-        image: media,
-      },
-      {
-        id: "3",
-        image: apps,
-      },
-      {
-        id: "4",
-        image: dogmarket,
-      },
-    ];
-    handleRandom(imageList);
-    let countAds = 0;
-    let startAds = setInterval(() => {
-      countAds++;
-      handleRandom(imageList);
-      count.current = countAds;
-      if (countAds === 5) {
-        clearInterval(startAds);
-      }
-    }, 2000);
-
-    return () => {
-      clearInterval(startAds);
-    };
-  }, []);
-
-  const progressBar = () => {
-    switch (count.current) {
-      case 1:
-        return 20;
-      case 2:
-        return 40;
-      case 3:
-        return 60;
-      case 4:
-        return 80;
-      case 5:
-        return 100;
-      default:
-        return 0;
-    }
-  };
 
   const addData = async (e) => {
     e.preventDefault();
-  
+
     const collectionUsersRef = collection(db, "users");
     const q = query(collectionUsersRef, where("uid", "==", user.uid));
-  
+
     try {
       const querySnapshot = await getDocs(q);
-  
+
       // Check if there is a document with the specified UID
       if (querySnapshot.size > 0) {
         const userDocRef = querySnapshot.docs[0].ref;
-  
+
         // Check if all fields are not empty before updating the document
         if (kind !== "" && species !== "" && dateOfBirth !== "" && habitat !== "" && description !== "") {
           // Update the document with the new data
@@ -107,14 +44,14 @@ const LeftSide = () => {
             description: description,
             // Add other fields if needed
           });
-  
+
           // Clear the form inputs
           setKind("");
           setSpecies("");
           setDateOfBirth("");
           setHabitat("");
           setDescription("");
-  
+
           console.log("Data updated successfully!");
         } else {
           throw new Error("All fields must be filled");
@@ -128,7 +65,7 @@ const LeftSide = () => {
       console.error(err.message);
     }
   };
-  
+
 
   return (
     <div className="flex flex-col h-screen bg-white pb-4 border-2 rounded-r-xl shadow-1g">
@@ -209,52 +146,7 @@ const LeftSide = () => {
         Submit
       </button>
 
-      <div className="flex flex-col">
-        <div className="flex items-center pb-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 mx-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
-            />
-          </svg>
-
-          <p className="font-roboto font-bold text-1g no-underline tracking-normal leading-none">
-            Hungary
-          </p>
-        </div>
-      </div>
-
-      <div className="flex flex-col">
-        <div className="flex items-center pb-4 flex">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 mx-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
-            />
-          </svg>
-
-          <p className="font-roboto font-bold text-1g no-underline tracking-normal leading-none">
-            React Developer
-          </p>
-        </div>
-      </div>
-      <div className="ml-2">
+      <div className="ml-20 mt-5">
         <p className="font-roboto font-bold text-lg no-underline tracking-normal leading-none py-2">
           Social Profiles
         </p>
@@ -271,16 +163,7 @@ const LeftSide = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center pt-4">
-        <p className="font-roboto font-bold text-lg no-underline tracking-normal leading-none py-2">
-          Random Ads
-        </p>
-        <div
-          style={{ width: `${progressBar()}%` }}
-          className="bg-green-600 rounded-xl h-1 mb-4"
-        ></div>
-        <img className="h-36 rounded-lg" src={data.image} alt="ads"></img>
-      </div>
+
     </div>
   );
 };
--- src/Componenets/Main/CommentSection.jsx
+++ src/Componenets/Main/CommentSection.jsx
@@ -7,68 +7,78 @@ import { db } from '../Firebase/firebase';
 import { PostReducer, postActions, postsStates } from '../AppContext/PostReducer'
 import Comment from "./Comment";
 
-const CommentSection = ({postId}) => {
-    const comment = useRef("")
-    const { user, userData } = useContext(AuthContext)
-    const commentRef = doc(collection(db, 'posts', postId, 'comments'))
-    const [state, dispatch] = useReducer(PostReducer, postsStates);
-    const { ADD_COMMENT, HANDLE_ERROR } = postActions
+const CommentSection = ({ postId }) => {
+  const comment = useRef("")
+  const { user, userData } = useContext(AuthContext)
+  const commentRef = doc(collection(db, 'posts', postId, 'comments'))
+  const [state, dispatch] = useReducer(PostReducer, postsStates);
+  const { ADD_COMMENT, HANDLE_ERROR } = postActions
 
-    const addComment = async (e) => {
-        e.preventDefault()
-        if(comment.current.value !== "" ) {
-            try {
-                await setDoc(commentRef, {
-                    id: commentRef.id,
-                    comment: comment.current.value,
-                    image: user?.photoURL,
-                    name: user?.displayName,
-                    timestamp: serverTimestamp(),
-                })
-                comment.current.value = ""
-            } catch (err) {
-                dispatch({ type: HANDLE_ERROR })
-                alert(err.message);
-                console.log(err.message)
-                console.log(user)
-            }
-        }
+  const addComment = async (e) => {
+    e.preventDefault();
+  
+    if (!comment || !comment.current) {
+      return; // Null check to avoid errors
     }
+  
+    if (comment.current.value !== "") {
+      try {
+        await setDoc(commentRef, {
+          id: commentRef.id,
+          comment: comment.current.value,
+          image: user?.photoURL,
+          name: user?.displayName,
+          timestamp: serverTimestamp(),
+        });
+  
+        if (comment && comment.current) {
+          comment.current.value = "";
+        }
+      } catch (err) {
+        dispatch({ type: HANDLE_ERROR });
+        alert(err.message);
+        console.error(err.message);
+      }
+    }
+  };
+  
+  
 
-    useEffect(() => {
-        const getComments = async () => {
-          try {
-            const collectionOfComments = collection(db, `posts/${postId}/comments`);
-            const q = query(collectionOfComments, orderBy("timestamp", "desc"));
-            onSnapshot(q, (doc) => {
-              dispatch({
-                type: ADD_COMMENT,
-                comments: doc.docs?.map((item) => item.data()),
-              });
-            });
-          } catch (err) {
-            dispatch({ type: HANDLE_ERROR });
-            alert(err.message);
-            console.log(err.message);
-          }
-        };
-        return () => getComments();
-      }, [postId, ADD_COMMENT, HANDLE_ERROR]);
 
-    return (
-        <div className='flex flex-col bg-white w-full py-2 rounded-b-3x1'>
-        <div className='flex items-center'>
-            <div className='mx-2'>
-                <Avatar size="sm" variant='circular' src={user?.photoURL || avatar}></Avatar>
-            </div>
-            <div className='w-full pr-2'>
-                <form className='flex items-center w-full' onSubmit={addComment}>
-                    <input name='comment' className='w-full rounded-2x1 outline-none border-0 p-2 bg-gray-100' ref={comment} placeholder='Write a comment...' type='text'></input>
-                    <button className='hidden' type="submit">Submit</button>
-                </form>
-            </div>
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const collectionOfComments = collection(db, `posts/${postId}/comments`);
+        const q = query(collectionOfComments, orderBy("timestamp", "desc"));
+        onSnapshot(q, (doc) => {
+          dispatch({
+            type: ADD_COMMENT,
+            comments: doc.docs?.map((item) => item.data()),
+          });
+        });
+      } catch (err) {
+        dispatch({ type: HANDLE_ERROR });
+        alert(err.message);
+        console.log(err.message);
+      }
+    };
+    return () => getComments();
+  }, [postId, ADD_COMMENT, HANDLE_ERROR]);
+
+  return (
+    <div className='flex flex-col bg-white w-full py-2 rounded-b-3x1'>
+      <div className='flex items-center'>
+        <div className='mx-2'>
+          <Avatar size="sm" variant='circular' src={user?.photoURL || avatar}></Avatar>
         </div>
-        {state?.comments?.map((comment, index) => {
+        <div className='w-full pr-2'>
+          <form className='flex items-center w-full' onSubmit={addComment}>
+            <input name='comment' className='w-full rounded-2x1 outline-none border-0 p-2 bg-gray-100' ref={comment} placeholder='Write a comment...' type='text'></input>
+            <button className='hidden' type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+      {state?.comments?.map((comment, index) => {
         return (
           <Comment
             key={index}
@@ -79,7 +89,7 @@ const CommentSection = ({postId}) => {
         );
       })}
     </div>
-    )
+  )
 }
 
 export default CommentSection
